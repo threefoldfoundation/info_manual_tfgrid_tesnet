b9f1ea9233602a883c6c42c182718fea1b8ada9c
kristof de spiegeleer <kristof@greenitglobe.com>
2020-09-10 06:15:55 +0200

    Update caddy_install.sh

--- scripts/caddy_install.sh
+++ scripts/caddy_install.sh
@@ -1,6 +1,9 @@
 set -ex
 cd /tmp
 
+echo "INSTALL CRYSTAL TOOLS & TFWEB"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/threefoldfoundation/websites/master/tools/install.sh)"
+
 if ! [ -x "$(command -v curl)" ]; then
     apt update
     apt install curl -y
