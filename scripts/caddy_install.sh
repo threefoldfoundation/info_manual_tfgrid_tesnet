1ec83d3fbf091400bc7be4d6629f11e601f67446
kristof de spiegeleer <kristof@greenitglobe.com>
2020-09-10 06:47:44 +0200

    Update caddy_install.sh

--- scripts/caddy_install.sh
+++ scripts/caddy_install.sh
@@ -1,4 +1,4 @@
-set -ex
+set -e
 cd /tmp
 
 echo "INSTALL CRYSTAL TOOLS & TFWEB"
@@ -10,10 +10,31 @@ if ! [ -x "$(command -v curl)" ]; then
 fi
 
 FILE=/tmp/caddy
-if [ ! -f "$FILE" ]; then
-    echo "$FILE does not exist yet will download."
-    curl --output /tmp/caddy https://caddyserver.com/api/download?os=linux&arch=amd64
-    chmod 770 caddy
+
+#check right version if not remove
+caddy version | grep -q "^v2."
+if [ ! $? == 0 ]; then
+    rm -f "$FILE"
 fi
 
+if [ ! -f "$FILE" ]; then
+    echo "$FILE does not exist yet will download."
+    curl --output /tmp/caddy 'https://caddyserver.com/api/download?os=linux&arch=amd64' ; echo "DONE"
+    chmod 770 /tmp/caddy
+    cp /tmp/caddy /usr/local/bin/caddy
+fi
+
+if ! [ -x "$(command -v caddy)" ]; then
+    echo "CADDY WAS NOT INSTALLED"
+    exit 1
+fi
+
+set +e
+caddy version | grep -q "^v2."
+if [ $? == 0 ]; then
+        echo "CADDY INSTALLED PROPERLY"
+else
+        echo "CADDY WAS NOT PROPERLY INSTALLED"
+        exit 1
+fi
 
